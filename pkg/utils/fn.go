f1add00f4f104fb1e7b01055e26f68f18288398b
buffge <1515888956@qq.com>
2020-06-30 06:52:25 +0800

    [update] randomBytes [add] randomStr

--- pkg/utils/fn.go
+++ pkg/utils/fn.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"math"
 	"math/rand"
 	"net"
 	"net/http"
@@ -8,17 +9,34 @@ import (
 	"time"
 )
 
+func RandomStr(lengthParam ...int) string {
+	length := 16
+	if len(lengthParam) > 0 {
+		length = lengthParam[0]
+	}
+	bytes := []byte{
+		'0', '1', '2', '3', '4', '5', '6', '7', '8', '9',
+		'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+		'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+		'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+		'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	}
+	var res []byte
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	for i := 0; i < length; i++ {
+		res = append(res, bytes[r.Intn(len(bytes))])
+	}
+	return string(res)
+}
 func RandomBytes(lengthParam ...int) []byte {
 	length := 16
 	if len(lengthParam) > 0 {
 		length = lengthParam[0]
 	}
-	str := "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	bytes := []byte(str)
 	var res []byte
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	for i := 0; i < length; i++ {
-		res = append(res, bytes[r.Intn(len(bytes))])
+		res = append(res, byte(r.Intn(math.MaxUint8)))
 	}
 	return res
 }
